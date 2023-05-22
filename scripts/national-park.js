9d5b370f85ff0dfa37938277e7ffd63d11a8052e
Kevin <kevin7herrera@gmail,com>
2023-05-21 21:53:52 -0400

    new javascript changes

---
+++ scripts/national-park.js
@@ -0,0 +1,17 @@
+"use strict";
+
+window.onload = () => {
+    let byLocation = document.getElementById("locationArray");
+    let parkType = document.getElementById("parkType");
+
+    byLocation.onclick = onByLocationClick
+}
+
+function onByLocationClick () {
+    for (let locationArrayy of locationsArray);
+    console.log(locationsArray);
+}
+
+function onDisplayInnerHTML () {
+    let html = document.getElementById("locationArray").innerHTML;
+}
